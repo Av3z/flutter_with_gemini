ed6e63a94837cf70b46aec4be662be6a29800849
Av3z <willyanf17@hotmail.com>
2024-04-26 21:57:29 -0300

    initial commit

---
+++ android/app/src/main/kotlin/com/wltisolution/flutter_with_gemini/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.wltisolution.flutter_with_gemini
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_with_gemini/src/app.dart';
+
+void main() {
+  runApp(const App());
+}
