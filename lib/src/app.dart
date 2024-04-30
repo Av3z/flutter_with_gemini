da88269e0cf1fbc1a441631849e6cab2bdd3da03
Av3z <willyanf17@hotmail.com>
2024-04-29 23:56:51 -0300

    chore: add home

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_with_gemini/src/pages/home_page.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Flutter With Gemini',
-      home: Container(),
+      home: HomePage(),
     );
   }
 }
